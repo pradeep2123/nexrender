c63b543a9608e6aa4141148581f36efd60d3eb73
pradeep_1996 <pradeep@crayond.co>
2020-03-29 14:13:57 +0530

    added

--- start.js
+++ start.js
@@ -3,7 +3,8 @@ const http = require("http");
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
-
+const express = require("express")
+var app = express()
 const {init, render} = require("@nexrender/core");
 
 /**
@@ -52,72 +53,110 @@ let server = http.createServer((req, res) => {
 /**
  * Renderer
  */
-server.listen(port, () => {
+app.listen(port, () => {
   console.log("Started local static server at port:", port);
 
   // addtional info about configuring project can be found at:
   // https://github.com/Inlife/nexrender/wiki/Project-model
-  let project = {
-    template: {
-      src: `http://localhost:${port}/assets/${aepxfile}`,
-      composition: "main",
-      frameStart: 0,
-      frameEnd: duration,
-    },
-    assets: [
-      // {
-      //   type: "image",
-      //   name: "background.jpg",
-      //   layerName: "background.jpg",
-      //   src: `https://picsum.photos/1280/720`,
-      //   filters: [{name: "cover", params: [1280, 720]}],
-      //   extension: "jpg",
-      // },
-      {
-        type: "image",
-        name: "nm.png",
-        layerName: "nm.png",
-        src: `http://localhost:${port}/assets/nm.png`,
-      },
-      {
-        type: "audio",
-        name: `audio.${audio}`,
-        layerName: `audio.${audio}`,
-        src: `http://localhost:${port}/assets/${mixfile}`,
-      },
+  // let project = {
+  //   template: {
+  //     src: `http://localhost:${port}/assets/${aepxfile}`,
+  //     composition: "main",
+  //     frameStart: 0,
+  //     frameEnd: duration,
+  //   },
+  //   assets: [
+  //     // {
+  //     //   type: "image",
+  //     //   name: "background.jpg",
+  //     //   layerName: "background.jpg",
+  //     //   src: `https://picsum.photos/1280/720`,
+  //     //   filters: [{name: "cover", params: [1280, 720]}],
+  //     //   extension: "jpg",
+  //     // },
+  //     {
+  //       type: "image",
+  //       name: "nm.png",
+  //       layerName: "nm.png",
+  //       src: `http://localhost:${port}/assets/nm.png`,
+  //     },
+  //     {
+  //       type: "audio",
+  //       name: `audio.${audio}`,
+  //       layerName: `audio.${audio}`,
+  //       src: `http://localhost:${port}/assets/${mixfile}`,
+  //     },
       
-      {
-        type: "data",
-        layerName: "artist",
-        property: "position",
-        value: [0, duration],
-        expression: `[5*time, ${duration}]`,
-      },
-      {
-        type: "data",
-        layerName: "track name",
-        property: "Source Text",
-        value: "lorem",
-      },
-    ],
-    actions: {
-      postrender: [
-        {
-          module: "@nexrender/action-encode",
-          output: "output.mp4",
-          preset: "mp4",
-        },
-        {
-          module: "@nexrender/action-copy",
-          input: "output.mp4",
-          output: process.cwd() +"/results/output.mp4",
-        },
-      ],
+  //     {
+  //       type: "data",
+  //       layerName: "artist",
+  //       property: "position",
+  //       value: [0, duration],
+  //       expression: `[5*time, ${duration}]`,
+  //     },
+  //     {
+  //       type: "data",
+  //       layerName: "track name",
+  //       property: "Source Text",
+  //       value: "lorem",
+  //     },
+  //   ],
+  //   actions: {
+  //     postrender: [
+  //       {
+  //         module: "@nexrender/action-encode",
+  //         output: "output.mp4",
+  //         preset: "mp4",
+  //       },
+  //       {
+  //         module: "@nexrender/action-copy",
+  //         input: "output.mp4",
+  //         output: process.cwd() +"/results/output.mp4",
+  //       },
+  //     ],
+  //   },
+  //   onChange: (job, state) => console.log("testing onChange:", state),
+  //   onRenderProgress: (job, value) =>
+  //     console.log("testing onRenderProgress:", value),
+  // };
+  
+});
+
+app.post("/add",function(req,res){
+  let project = {
+    "template": {
+      "src": "https://firebasestorage.googleapis.com/v0/b/the-wedding-factory.appspot.com/o/Project.aep?alt=media&token=46096bbf-5a0c-46a2-8ef7-9f19dacaf343",
+      "composition": "main",
+      "continueOnMissing": true
     },
-    onChange: (job, state) => console.log("testing onChange:", state),
-    onRenderProgress: (job, value) =>
-      console.log("testing onRenderProgress:", value),
-  };
+    "assets": [
+  {
+              "src": "https://firebasestorage.googleapis.com/v0/b/the-wedding-factory.appspot.com/o/elegant-white-card-with-white-floral-decoration_1361-1269.jpg?alt=media&token=d981a50f-caeb-456d-8366-42b20343cd67",
+              "type": "image",
+              "layerName": "background.jpg"
+          },{
+              "type": "data",
+              "layerName": "Text1",
+              "property": "Source Text",
+              "expression": "'Hello world'"
+          }
+  
+    ],
+    "actions": {
+      "postrender": [
+        {
+          "module": "@nexrender/action-encode",
+          "preset": "mp4",
+          "output": "encoded.mp4"
+        },
+        {
+          "module": "@nexrender/action-copy",
+          "input": "encoded.mp4",
+          "output": "/results/output.mp4"
+        }
+      ]
+    }
+  }
 
   const settings = {
     logger: console,
@@ -131,9 +170,8 @@ server.listen(port, () => {
   render(project, init(settings))
     .then((data) => {
       // success
-      server.close();
       console.log("rendering finished");
-      http.send({
+      res.send({
         type:"success",
         data:data
       })
@@ -141,7 +179,10 @@ server.listen(port, () => {
     .catch(err => {
       // error
       console.error(err);
-      server.close();
+      res.send({
+        type:"err",
+        data:err
+      })
     });
-});
+})
 
